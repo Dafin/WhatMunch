b915411896709693cf91b7481c63e4ba0291a2b0
Dafin <dafin@users.noreply.github.com>
2017-12-08 00:53:36 +0000

    - Refactored html output to its own method

--- app.rb
+++ app.rb
@@ -4,25 +4,29 @@ require "sinatra/reloader"
 $munch = []
 
 get "/" do
-
   $munch << params
+  template
 
-"
-<html>
-<title>WhatMunch</title>
-<body>
-<form action='/' method='get'>
-What: <input name='what'>
-Cost: <input name='cost'>
-<button type='submit'>add munch</button>
-</form>
-#{ $munch.inspect }
-</body>
-</html>
-"
 end
 
 
 get "/places" do
  "This will list food vendors and their distance away"
+end
+
+def template
+  "
+  <html>
+  <title>WhatMunch</title>
+  <body>
+  <form action='/' method='get'>
+  What: <input name='what'>
+  Cost: <input name='cost'>
+  <button type='submit'>add munch</button>
+  </form>
+  #{ $munch.inspect }
+  </body>
+  </html>
+  "
+
 end
