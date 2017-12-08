4956de1e4a94a9961855e96be39ea5ca69d43808
Dafin <dafin@users.noreply.github.com>
2017-12-08 00:22:09 +0000

    - Added form for data entry

--- app.rb
+++ app.rb
@@ -2,5 +2,21 @@ require "sinatra"
 require "sinatra/reloader"
 
 get "/" do
-"Munch Time!"
+"
+<html>
+<body>
+<form action='/' method='get'>
+What: <input name='what'>
+Cost: <input name='cost'>
+<button type='submit'>add munch</button>
+</form>
+<!-- food list goes here -->
+</body>
+</html>
+"
+end
+
+
+get "/places" do
+ "This will list food vendors and their distance away"
 end
